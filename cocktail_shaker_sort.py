34c2ac4bc20fb79b4f6e1428aad341e5498bac9c
RebelAndroid <47703105+RebelAndroid@users.noreply.github.com>
2019-02-16 20:50:21 -0500

    added cocktail shaker sort

---
+++ cocktail_shaker_sort.py
@@ -0,0 +1,19 @@
+def cocktail_shaker_sort(a):
+    sorted = False
+    while(not sorted):
+        sorted = True
+        for i in range(len(a) - 1):
+            print(i)
+            if a[i] > a[i+1]:
+                temp = a[i]
+                a[i] = a[i+1]
+                a[i+1] = temp
+                sorted = False
+        for i in range(len(a) - 1, 1, -1):
+            print(i)
+            if a[i] < a[i-1]:
+                temp = a[i]
+                a[i] = a[i-1]
+                a[i-1] = temp
+                sorted = False
+    return a
