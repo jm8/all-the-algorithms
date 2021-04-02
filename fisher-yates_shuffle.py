034a2bcfe0cfd60e35f0ccc0ab348c28d1573613
RebelAndroid <47703105+RebelAndroid@users.noreply.github.com>
2021-04-02 15:11:31 -0400

    can't spell

--- fisher-yates_shuffle.py
+++ fisher-yates_shuffle.py
@@ -1,6 +1,6 @@
 from random import randint
 
-# improved version of fischer-yates shuffle by Durstenfeld
+# improved version of Fisher-Yates shuffle by Durstenfeld
 def shuffle(array):
     for i in range(len(array) - 1, 0, -1):
         j = randint(0, i)
