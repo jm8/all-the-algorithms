e5ae081ad4f6a7d07879306655e817b86126d12b
RebelAndroid <47703105+RebelAndroid@users.noreply.github.com>
2021-04-02 15:08:45 -0400

    took me 2 whole years to write
    /s

---
+++ fischer-yates_shuffle.py
@@ -0,0 +1,11 @@
+from random import randint
+
+# improved version of fischer-yates shuffle by Durstenfeld
+def shuffle(array):
+    for i in range(len(array) - 1, 0, -1):
+        j = randint(0, i)
+        # swap array elements at i and j
+        t = array[i]
+        array[i] = array[j]
+        array[j] = t
+    return array
